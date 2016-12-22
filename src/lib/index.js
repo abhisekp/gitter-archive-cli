d25e805210502056af2c6c1409844ef596d0641f
Abhisek Pattnaik <abhisekp@engineer.com>
2016-12-23 03:45:50 +0530

    Close logger on idle for 2 minutes

--- src/lib/index.js
+++ src/lib/index.js
@@ -33,7 +33,14 @@ const getNextGitterClient = (() => {
 	};
 })();
 
+let loggerCloseTimeoutId;
 const fetchAllRoomMessagesAsync = async ({roomId, beforeId, skip = 0, ...restparam} = {}) => {
+	// every time check if a timeout is set to close the logger
+	// if messages are still being retrieved, then clear timeout
+	if (loggerCloseTimeoutId) {
+		clearTimeout(loggerCloseTimeoutId);
+	}
+
 	const {
 		groupId, roomUri, hasErrors = false, 
 		errorWriter, messageWriter, store, messageCount: totalMessageCount = 0,
@@ -63,12 +70,15 @@ const fetchAllRoomMessagesAsync = async ({roomId, beforeId, skip = 0, ...restpar
 			const errorMessage = hasErrors ? 'with errors' : 'with no errors';
 			logger.info(`Room: ${roomUri} (${roomId}) archive completed ${errorMessage}.`);
 			store.updateRoom({id: roomId}, 'isArchived', true);
-			logger.close();
+			
+			// close logger after 120 seconds (2 mins) so that it is immune to penalty of extra 1 minute
+			loggerCloseTimeoutId = setTimeout(::logger.close, 120e3 /* 120 seconds */);
+			
 			return undefined;
 		}
 
 		const __beforeId = getEarliestMessageId(messages);
-		
+
 		store.updateRoom({id: roomId}, 'beforeId', __beforeId);
 		store.updateRoom({id: roomId}, 'skip', skip);
 
@@ -79,7 +89,6 @@ const fetchAllRoomMessagesAsync = async ({roomId, beforeId, skip = 0, ...restpar
 	} catch (err) {
 		console.error(err);
 		saveError({writer: errorWriter, error: err, roomId, beforeId, skip, roomUri});
-		logger.close();
 	}
 };
 
